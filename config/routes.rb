2bebf421d6445984874b70a8b7edc0624b4848be
taiki011609 <s.taiki011609@gmail.com>
2020-07-18 12:13:35 +0900

    Tasksコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+
+  # 上記のルーティングを一括で設定
+  # root to: は'/'にアクセスした時のルーティング
+  resources :tasks
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
