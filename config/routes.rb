c324a4aeba0e05dd041dc5e2f5a48f9afd144b20
taiki011609 <s.taiki011609@gmail.com>
2020-07-19 18:45:23 +0900

    User管理作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    # get 'users/new'
+    # get 'users/edit'
+    # get 'users/show'
+    # get 'users/index'
+    resources :users
+  end
+
   # get 'tasks/index'
   # get 'tasks/show'
   # get 'tasks/new'
   # get 'tasks/edit'
-
   # 上記のルーティングを一括で設定
   # root to: は'/'にアクセスした時のルーティング
   resources :tasks
