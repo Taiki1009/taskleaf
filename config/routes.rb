099ec88d8af4669d10abe02068077647c37277d5
taiki011609 <s.taiki011609@gmail.com>
2020-08-24 12:52:41 +0900

    routesメモ

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,18 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  # /loginを他のメソッドでも呼び出す場合に使用する
+  # match '/login', to: 'sessions#create', via: [:post, :patch, :delete]
+
   namespace :admin do
-    # get 'users/new'
-    # get 'users/edit'
-    # get 'users/show'
-    # get 'users/index'
     resources :users
   end
-
-  # get 'tasks/index'
-  # get 'tasks/show'
-  # get 'tasks/new'
-  # get 'tasks/edit'
-  # 上記のルーティングを一括で設定
-  # root to: は'/'にアクセスした時のルーティング
   resources :tasks
   root to: 'tasks#index'
+  
+  # resourcesを１つのメソッドのみで使用したい場合
+  # resource :tasks, only: [:index]
+  # resources :tasks, except: [:delete, :patch, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
