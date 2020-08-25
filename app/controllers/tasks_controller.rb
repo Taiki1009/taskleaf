895a349d217a5ad38e1b455b5ec638adbf4b3e0c
taiki011609 <s.taiki011609@gmail.com>
2020-08-25 13:43:01 +0900

    タスク検索・ソート機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
 
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def show
