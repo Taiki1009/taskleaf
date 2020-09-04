af262e30cbb666814196e1f75dc5623d64539175
taiki011609 <s.taiki011609@gmail.com>
2020-09-04 21:14:50 +0900

    Webpackerを使ったJS＋Reactの導入

--- app/javascript/javascripts/task.js
+++ app/javascript/javascripts/task.js
@@ -1,15 +1,15 @@
 // ホバーしたタスクのbackground-colorを青色にする
-// document.addEventListener('turbolinks:load', function() {
-//     document.querySelectorAll('td').forEach(function (td) {
-//         td.addEventListener('mouseover', function (e) {
-//             e.currentTarget.style.backgroundColor = '#eff';
-//         });
+document.addEventListener('turbolinks:load', function() {
+    document.querySelectorAll('td').forEach(function (td) {
+        td.addEventListener('mouseover', function (e) {
+            e.currentTarget.style.backgroundColor = '#eff';
+        });
 
-//         td.addEventListener('mouseout', function (e) {
-//             e.currentTarget.style.backgroundColor = ''; 
-//         });
-//     });
-// });
+        td.addEventListener('mouseout', function (e) {
+            e.currentTarget.style.backgroundColor = ''; 
+        });
+    });
+});
 // Ajaxイベントを使った非同期
 // document.addEventListener('turbolinks:load', function() {
 //     document.querySelectorAll('.delete').forEach(function (a) {
