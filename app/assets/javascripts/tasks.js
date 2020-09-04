11bdabd490ee60f70e2053b72186bf658c1e1d6a
taiki011609 <s.taiki011609@gmail.com>
2020-09-04 18:22:55 +0900

    js(非同期)でのタスク削除〜非表示

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,22 @@
+// ホバーしたタスクのbackground-colorを青色にする
+document.addEventListener('turbolinks:load', function() {
+    document.querySelectorAll('td').forEach(function (td) {
+        td.addEventListener('mouseover', function (e) {
+            e.currentTarget.style.backgroundColor = '#eff';
+        });
+
+        td.addEventListener('mouseout', function (e) {
+            e.currentTarget.style.backgroundColor = ''; 
+        });
+    });
+});
+// Ajaxイベントを使った非同期
+// document.addEventListener('turbolinks:load', function() {
+//     document.querySelectorAll('.delete').forEach(function (a) {
+//         a.addEventListener('ajax:success', function () {
+//             let td = a.parentNode;
+//             let tr = td.parentNode;
+//             tr.style.display = 'none';
+//         });
+//     });
+// });
